4c19e9db9092e92a052df4e5df7a27cd8d55f84a
Vovkkk-cyber <vov.blindowscky@yandex.ru>
2025-05-19 21:01:47 +0300

    выполнил задание

---
+++ script.js
@@ -0,0 +1,58 @@
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+let ageIphone = 2007;
+alert(ageIphone);
+let nameJs = 'Брэндан Эйх'
+alert(nameJs);
+let x = 10;
+let b = 2;
+let sum = x + b;
+let dif = x - b;
+let mul = x * b;
+let share = x / b;
+alert(sum);
+alert(dif);
+alert(mul);
+alert(share);
+let c = 2;
+let result =c**5;
+alert(result);
+let v = 9;
+let z = 2;
+alert(v/z);
+
+let num = 1;
+num = num + 5;
+num = num - 3;
+num = num * 7;
+num = num / 3;
+num = num + 1;
+num = num - 1;
+alert(num);
+
+let age = Number(prompt('Сколько вам лет?'));
+alert(age);
+
+const user = {
+    name: 'Vlad',
+    ageuser: 28,
+    isAdmin: true
+}
+
+let Name = prompt('Как вас зовут?');
+alert(`Привет, ${Name}`);
+
+let number = Number(prompt('Введите любое число'));
+result1 = number * 2;
+alert(result1);
+result2 = result1 + 10;
+alert(result2);
+result3 = result2 / 2;
+alert(result3);
+result4 = result3 - number;
+alert(result4)
+let ancer = 5;
+alert(ancer)
+
