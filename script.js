5891462ee7fa07358c3eea0774191ad7b25abb7b
Vovkkk-cyber <vov.blindowscky@yandex.ru>
2025-05-22 15:36:44 +0300

    задание5

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
-let a = '2';
-let b = '3';
-alert(+a + +b)
-
-
+let monthNumber = Number(prompt('Введите порядковый номер месяца'));
+switch (monthNumber) {
+    case 1:
+        console.log('Зима')
+        break;
+    case 2:
+        console.log('Зима')
+        break;
+    case 3:
+        console.log('Весна')
+        break;
+    case 4:
+        console.log('Весна')
+        break;
+    case 5:
+        console.log('Весна')
+        break;
+    case 6:
+        console.log('Лето')
+        break;
+    case 7:
+        console.log('Лето')
+        break;
+    case 8:
+        console.log('Лето')
+        break;
+    case 9:
+        console.log('Осень')
+        break;
+    case 10:
+        console.log('Осень')
+        break;
+    case 11:
+        console.log('Осень')
+        break;
+    case 12:
+        console.log('Зима')
+        break;
+    default:
+        console.log('Ввели что-то не то')
+        break;
+}
