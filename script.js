30160a69c5ae287961d93578d896ef5148a1bc4e
Vovkkk-cyber <vov.blindowscky@yandex.ru>
2025-05-22 16:20:51 +0300

    доп.задание3

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let clientOS = Number(prompt('Выберите корректное для вас: 0 - iOS; 1 - Android'));
-switch (clientOS) {
-    case 0:
-        console.log('Установите версию приложения для iOS по ссылке');
-        break;
-    case 1:
-        console.log('Установите версию приложения для Android по ссылке');
-        break;
-    default:
-        console.log('ввели что-то не то');
-        break;
+let clientDevice = Number(prompt('Введите год выпуска телефона'));
+if (clientOS === 0 && clientDevice >= 2015) {
+    console.log('Установите версию приложения для iOS по ссылке');
+} else if (clientOS === 0 && clientDevice < 2015) {
+    console.log('Установите облегченную версию приложения для iOS по ссылке');
+} else if (clientOS === 1 && clientDevice >= 2015) {
+    console.log('Установите версию приложения для Android по ссылке');
+} else if (clientOS === 1 && clientDevice < 2015) {
+    console.log('Установите облегченную версию приложения для Android по ссылке');
+} else {
+    console.log('ввели что-то не то');
 }
