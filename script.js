6467ba9afba209a89190e8c6aff0d60cfd60eb0c
Vovkkk-cyber <vov.blindowscky@yandex.ru>
2025-05-22 16:03:16 +0300

    доп.задание2

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-let number = Number(prompt('Пожалуйста, введите любое число'));
-let message = (number % 2 === 0)?'Число четное':'Число нечетное';
-alert(message)
+let clientOS = Number(prompt('Выберите корректное для вас: 0 - iOS; 1 - Android'));
+switch (clientOS) {
+    case 0:
+        console.log('Установите версию приложения для iOS по ссылке');
+        break;
+    case 1:
+        console.log('Установите версию приложения для Android по ссылке');
+        break;
+    default:
+        console.log('ввели что-то не то');
+        break;
+}
