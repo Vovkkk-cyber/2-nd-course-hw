638fe0747aa9d41134749983b9abf73727b6ea0f
Vovkkk-cyber <vov.blindowscky@yandex.ru>
2025-05-27 10:16:55 +0300

    домашка по циклам

--- script.js
+++ script.js
@@ -1,75 +1,74 @@
-let password = 'пароль';
-let passworduse = prompt('Введите пароль');
-let message = (password === passworduse)?'Пароль введен верно':'Пароль введен неправильно';
-alert(message)
-
-let c = 2;
-let message1 = (c > 0 && c < 10)?'верно':'неверно';// с 0, 10, -3 проверял работает
-alert(message1)
-
-let d = 80;
-let e = 120;
-let message3 = (d > 100 || e > 100)?'Верно':'Неверно';
-alert(message3)
-
-let a = '2';
-let b = '3';
-alert(+a + +b)
-
-let monthNumber = Number(prompt('Введите порядковый номер месяца'));
-switch (monthNumber) {
-    case 1:
-    case 2:
-    case 12:
-    console.log('Зима')
-        break;
-    case 3:
-    case 4:
-    case 5:
-    console.log('Весна')
-        break;
-    case 6:
-    case 7:
-    case 8:
-    console.log('Лето')
-        break;
-    case 9:
-    case 10:
-    case 11:
-    console.log('Осень')
-        break;
-    default:
-        console.log('Ввели что-то не то')
-        break;
+let i = 1;
+while (i <= 2) {
+    console.log('Привет');
+    i++;
 }
 
-let number = Number(prompt('Пожалуйста, введите любое число'));
-let message2 = (number % 2 === 0)?'Число четное':'Число нечетное';
-alert(message2)
-
-let clientOS = Number(prompt('Выберите корректное для вас: 0 - iOS; 1 - Android'));
-switch (clientOS) {
-    case 0:
-        console.log('Установите версию приложения для iOS по ссылке');
-        break;
-    case 1:
-        console.log('Установите версию приложения для Android по ссылке');
-        break;
-    default:
-        console.log('ввели что-то не то');
-        break;
+let a = 1;
+while (a <= 5) {
+    console.log(a);
+    a++;
 }
 
-let clientOS1 = Number(prompt('Выберите корректное для вас: 0 - iOS; 1 - Android'));
-let clientDevice = Number(prompt('Введите год выпуска телефона'));
-if (clientOS1 === 0 && clientDevice >= 2015) {
-    console.log('Установите версию приложения для iOS по ссылке');
-} else if (clientOS1 === 0 && clientDevice < 2015) {
-    console.log('Установите облегченную версию приложения для iOS по ссылке');
-} else if (clientOS1 === 1 && clientDevice >= 2015) {
-    console.log('Установите версию приложения для Android по ссылке');
-} else if (clientOS1 === 1 && clientDevice < 2015) {
-    console.log('Установите облегченную версию приложения для Android по ссылке');
-} else {
-    console.log('ввели что-то не то');
+let z = 7;
+while (z <= 22) {
+    console.log(z);
+    z++;
 }
+
+const obj = {
+    "Коля": 200,
+    "Вася": 300,
+    "Петя": 400
+}
+
+for (const key in obj) {
+    console.log(key,obj[key]);
+}
+
+let n = 1000;
+let num = 1;
+
+do {
+    n /= 2;
+    console.log(n);
+    console.log(num);
+    num++;
+} while (n >= 50);
+
+for (let friday = 3; friday <= 31; friday += 7) {
+    alert("Сегодня пятница, " +friday+ "-е число. Необходимо подготовить отчет.");
+}
+
+let k = 100;
+let interations = 1;
+
+do {
+    k -= 7;
+    interations++;
+} while (k > 0);
+alert("Получившееся число " +k);
+alert("Количество вычетаний " +interations);
+
+const months = [
+    "Январь", "Фераль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"
+]
+for(let m = 0; m < months.length; m++) {
+    console.log(`${m + 1}. ${months[m]}`)
+}
+
+const object = {
+    "Название": "Книга",
+    "Автор": "Автор книги",
+    "Год издания": 2005,
+    "Жанр": "Жанр книги"
+}
+
+for (let key in object) {
+    console.log(`${key}:${object[key]}`);
+}
+
+const min = Math.min(...[
+    1, 20, 30, 43, -4, 93, 29, 883, -83, 3333
+])
+console.log(min)
