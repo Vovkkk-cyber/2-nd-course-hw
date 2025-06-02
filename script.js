aae556a125601f4e2b5d1bdf19a6f0c2b9f86e32
Vovkkk-cyber <vov.blindowscky@yandex.ru>
2025-06-02 18:01:14 +0300

    домашка по массивам

--- script.js
+++ script.js
@@ -1,125 +1,224 @@
+// Игра 1
 function guessNumber() {
-    let userNumber = Number(prompt('Угадай число от 1 до 100'));
+    let userInput = prompt('Угадай число от 1 до 100');
+    let userNumber = Number(userInput);
     const rightNumber = Math.floor(Math.random() * 100) + 1;
     while (userNumber !== rightNumber){
         if (userNumber > rightNumber) {
             alert('Не угадал, число меньше');
-            userNumber = Number(prompt('Угадай число от 1 до 100'));
-        }  else if (userNumber === 0) {
-            alert('Пока');
+            userInput = prompt('Угадай число от 1 до 100');
+            userNumber = Number(userInput)
+        }  else if (userInput === null) {
+            alert('Пока, приходи ещё.');
             break;
         } else if (userNumber < rightNumber) {
             alert('Не угадал, число больше');
-            userNumber = Number(prompt('Угадай число от 1 до 100'));
+            userInput = prompt('Угадай число от 1 до 100');
+            userNumber = Number(userInput);
         }  else {
             alert('Ввел что-то не то');
-            userNumber = Number(prompt('Угадай число от 1 до 100'));
+            userInput = prompt('Угадай число от 1 до 100');
+            userNumber = Number(userInput);
         }
     }
     if (userNumber === rightNumber) {
-            alert('Угадал, молодец');
+            alert('Угадал, молодец!');
         }
+}
+// Игра 2
+
+function simpleArith() {
+    function generateRandomArrey() {
+            const operation = ["+", "-", "*", "/"];
+
+        function generateRandomExample() {
+            const firstNumb = Math.floor(Math.random() * 10) + 1;
+            const secondNumb = Math.floor(Math.random() * 10) + 1;
+            const randomAction = Math.floor(Math.random() * operation.length);
+            const randomOperation = operation[randomAction];
+        
+            if (randomOperation === "/") {
+                const div = firstNumb * secondNumb;
+                return `${div} ${randomOperation} ${secondNumb}`;
+            }
+            return `${firstNumb} ${randomOperation} ${secondNumb}`;
+        }
+        function generateExample(count) {
+            const example = [];
+            for (let i = 0; i < count; i++) {
+                example.push(generateRandomExample());
+            }
+            return example;
+        }
+        return generateExample(4);
+    }
+    
+    const showExample = generateRandomArrey();
+    console.log(showExample);
+
+        for(const exap of showExample) {
+            const userInput = prompt(`Реши пример ${exap}`);
+            const userAnswer = Number(userInput);
+            const rightAnswer = eval(exap);
+            
+            if (userInput === null) {
+                alert('Пока, приходи ещё.');
+                break;
+            }
+            if (userAnswer !== rightAnswer) {
+                alert('Посчитал неправильно. Правильный ответ '+rightAnswer)
+            }
+            if (!isNaN(userAnswer) && userAnswer === rightAnswer) {
+                    alert("верный ответ, молодец!")
+        }
+}
 }
 
 // Задание 1
-function compar(a, b) {
-    if (a > b){
-    return a;
-    } else {
-        return b;
-    }
-}
-console.log(compar(4,2));
-console.log(compar(4,8));
-console.log(compar(6,6));
+const numbers = [1, 5, 4, 10, 0, 3];
 
+ for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] !== 10) {
+        console.log(numbers[i]);
+    } else {
+        console.log(numbers[i]);
+        break;
+    }
+ }
 // Задание 2
-function diff(n) {
-    if (n%2 == 0) {
-        return console.log('число четное');
-    } else {
-        return console.log('число нечетное');
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] === 4) {
+        console.log(i);
+        break;
     }
 }
-
-diff(33);
-diff(66);
 
 // Задание 3
-function degree(n) {
-    return n**2;
-}
-console.log(degree(5));
 
- function box(n) {
-    let m = n**2;
-    return m;
- }
- 
-console.log(box(4));
+let mass = [1, 3, 5, 10, 20];
+mass = mass.join(' ');
+console.log(mass);
+
 
 // Задание 4
-function greeting() {
-let userAge = Number(prompt('Введите свой возраст'));
-    if (userAge < 0) {
-        alert('Вы ввели неправильное значение');
-    } else if (userAge >= 0 && userAge <= 12) {
-        alert('Привет, друг!');
-    } else if (userAge >= 13) {
-        alert('Добро пожаловать!');
-    } else {
-        alert('Вы ввели неправильное значение');
-    }
-}
-greeting()
 
+const arr = [
+    [1, 1, 1],
+    [1, 1, 1],
+    [1, 1, 1]
+];
+
+    for ( let i = 0;i < arr.length; i++) {
+      if (i === 0) {
+         console.log(arr[i]);
+      } else {
+         console.log(arr[i]);
+      }
+   }
 // Задание 5
-function mult(a, b) {
-    a = Number(prompt('Введите 1-ое число'));
-    b = Number(prompt('Введите 2-ое число'));
-    if ( isNaN(a) || isNaN(b)) {
-        return 'Одно или оба значения не являются числом';
-    } else {
-        return 'Произведение чисел равно '+a * b;
-    }
-}
-alert(mult());
+
+
+const mas = [1, 1, 1];
+mas.push(2, 2, 2);
+
+console.log(mas);
 
 // Задание 6
-function transorm(n) {
-    n = Number(prompt('Введите число'));
-    if ( isNaN(n)) {
-        return 'Переданный параметр не является числом';
-    } else {
-        return 'n в кубе равняется '+n**2;
-    }
-}
-alert(transorm());
+
+const masss = [9, 8, 7, 'a', 6, 5];
+masss.sort();
+const dMasss = masss.filter(element => typeof element === 'number');
+console.log(dMasss);
 
 
 // Задание 7
 
-const circle1 = {
-    radius: 20,
-    getArea() {
-        return 'площадь круга равна ' +Math.PI * (this.radius**2);
-    },
-    getPerimeter() {
-        return 'периметр окружности равен ' +Math.PI * this.radius * 2;
-    }
+let userNumb = Number(prompt('Угадай число из массива'));
+let isFound = dMasss.includes(userNumb);
+if (isFound === true) {
+    alert('Угадал');
+} else {
+    alert('Неугадал');
 }
 
-const circle2 = {
-    radius: 50,
-    getArea() {
-        return 'площадь круга равна ' +Math.PI * (this.radius**2);
-    },
-    getPerimeter() {
-        return  'периметр окружности равен ' +Math.PI * this.radius * 2;
+
+// Задание 8
+
+let text = 'abcdef';
+let splitText = text.split('').reverse().join('')
+console.log(splitText);
+
+
+
+// Задание 9
+
+let arrrey = [[1, 2, 3],[4, 5, 6]];
+console.log(arrrey.reduce((flat, current) => flat.concat(current),));
+
+// Задание 10
+
+var masNumbers = [];
+var N = 10;
+
+for (var i = 0; i < N; i++ ) {
+    masNumbers.push(Math.floor(Math.random() * 10));
+}
+console.log(masNumbers);// для наглядности добавил
+
+for (var i = 0; i < masNumbers.length; i++) {
+    if (i === masNumbers.length - 1) {
+        break;
     }
+console.log(masNumbers[i] + masNumbers[i + 1]);
+
 }
 
-console.log(circle1.getArea())
-console.log(circle1.getPerimeter())
-console.log(circle2.getArea())
-console.log(circle2.getPerimeter())
+// Задание 11
+
+const boxMasNumbers = masNumbers.map(masNumber => masNumber**2);
+console.log(boxMasNumbers);
+
+
+// Задание 12
+
+let length = ["Dima", "Vlad", "Vova", "Micha"].map(item => item.length);
+
+console.log(length);
+
+
+// Задание 13
+
+const arreey = [2, -2, 5, -6, 10, -33];
+
+const filtrArreey = arreey.filter(arreey => arreey < 0);
+
+console.log(filtrArreey);
+
+
+// Задание 14
+
+var massNumbers = [];
+var A = 10;
+
+for (var i = 0; i < A; i++ ) {
+    massNumbers.push(Math.floor(Math.random() * 10));
+}
+console.log(massNumbers);
+
+let newMassNumbers = massNumbers.filter(massNumbers => massNumbers % 2 === 0);
+console.log(newMassNumbers);
+
+
+// Задание 15
+
+var arreyNumbers = [];
+var M = 6;
+
+for (var i = 0; i < M; i++ ) {
+    arreyNumbers.push(Math.floor(Math.random() * 10));
+}
+console.log(arreyNumbers) // добавил для наглядности
+
+let midArreyNumbers = arreyNumbers.reduce((acc, item) => acc + item, 0) / M;
+console.log(midArreyNumbers);
